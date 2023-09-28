b665155cb02e6b842d69173f770d5ddc1cabb65e
anicrob <arobbins@adaptavist.com>
2023-09-28 09:33:40 -0400

    added formatting to index.js

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ const script = async () => {
       const index = projectKeys.indexOf("CSPV3X");
       projectKeys.splice(index, 1);
     }
-    console.log("✅ All project keys have been found and CSPV3X was taken out of the list\n")
+    console.log(
+      "✅ All project keys have been found and CSPV3X was taken out of the list\n"
+    );
     const issueKeys = await getJiraIssueIds(projectKeys);
-    console.log("✅ All issues in the projects you have access to have been collected. Starting to delete issues now...\n")
+    console.log(
+      "✅ All issues in the projects you have access to have been collected. Starting to delete issues now...\n"
+    );
     await deleteTickets(issueKeys);
     console.log(
       "✅ The issues have been deleted. Please read the console for any errors that might have occurred.\n"
